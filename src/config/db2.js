a2150321b28029ca600e4d5429d9cc78e083a60e
Uladzislau <vladplakhotnikk@gmail.com>
2024-11-23 20:52:32 +0100

    feat: updatedd config for postgresql db

--- src/config/db2.js
+++ src/config/db2.js
@@ -1,22 +1,15 @@
 const { Pool } = require("pg");
 
-// Настройки подключения
 const pool = new Pool({
-  user: "droplock_user", // Имя пользователя из нового сервера
-  host: "dpg-ct11ujt2ng1s73e3deu0-a.oregon-postgres.render.com", // Хост нового сервера
-  database: "droplock", // Имя базы данных на новом сервере
-  password: "fWD167Ds6xT1V2eHK5f66j3snydtSQNZ", // Пароль из настроек нового сервера
-  port: 5432, // Порт PostgreSQL
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Разрешает небезопасные SSL-сертификаты
+    rejectUnauthorized: false,
   },
 });
 
 const connectDB2 = async () => {
   try {
-    // Проверяем подключение
     await pool.connect();
-    console.log("✅ PostgreSQL Connected...");
   } catch (error) {
     console.error("❌ Error connecting to PostgreSQL:", error.message);
     process.exit(1);
