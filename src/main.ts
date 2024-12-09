ebd2dc418e7b0651b0c9c6c0340f020a717e3298
Uladzislau <vladplakhotnikk@gmail.com>
2024-12-09 14:51:33 +0100

    chore: updated cors

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://droplock-frontend.vercel.app/',
-      'https://droplock-frontend.onrender.com/',
+      'https://droplock-frontend.vercel.app',
+      'https://droplock-frontend.onrender.com',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
